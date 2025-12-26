8bcf466416b17d84bedccdf15adc45798b0c811c
annawjst <anna@wjst.de>
2025-12-26 15:07:14 +0100

    changed config file to deploy

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/projection_mapping_react_app/" : "/",
   server: {
     host: "::",
     port: 8080,
